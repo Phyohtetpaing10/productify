0cadd1a537d0eadb930da62ab0e1c2af97438171
Phyo Htet Paing <phyohtetpaing200303@gmail.com>
2026-01-10 03:52:13 +0700

    fix error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,7 +26,7 @@ if (ENV.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
